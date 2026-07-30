cd15e8a5725aea0654b49bae5a7167c9de472b25
Shashank Kumar <shashank.cm.kumar@gmail.com>
2026-07-30 23:03:42 +0530

    Few minor changes in structure

---
+++ src/databases/vehicle_databases.py
@@ -0,0 +1,42 @@
+# Vehicle Database for STRIDE
+
+vehicle_database = [
+    {
+        "vehicle_id": 1,
+        "vehicle_name": "THeMIS",
+        "manufacturer": "Milrem Robotics",
+        "country": "Estonia",
+        "payload_capacity": 1200,
+        "battery": "Hybrid Electric",
+        "ground_clearance": 600,
+        "max_speed": 20,
+        "turning_radius": 0,
+        "terrain": ["Rocky", "Mud", "Sand", "Grass"]
+    },
+
+    {
+        "vehicle_id": 2,
+        "vehicle_name": "Mission Master XT",
+        "manufacturer": "Rheinmetall",
+        "country": "Canada",
+        "payload_capacity": 1000,
+        "battery": "Hybrid",
+        "ground_clearance": 500,
+        "max_speed": 40,
+        "turning_radius": 0,
+        "terrain": ["Mud", "Sand", "Snow", "Rocky"]
+    },
+
+    {
+        "vehicle_id": 3,
+        "vehicle_name": "MUTT",
+        "manufacturer": "General Dynamics",
+        "country": "USA",
+        "payload_capacity": 272,
+        "battery": "Electric",
+        "ground_clearance": 350,
+        "max_speed": 24,
+        "turning_radius": 0,
+        "terrain": ["Grass", "Rocky", "Sand"]
+    }
+]
